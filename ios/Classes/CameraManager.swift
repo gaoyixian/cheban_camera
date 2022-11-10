96077a0ca5c4a9a9ed8812069d6a71fd9d33a274
吴文炜 <wuwenwei@cz.com>
2022-11-10 19:38:13 +0800

    时间测试2

--- ios/Classes/CameraManager.swift
+++ ios/Classes/CameraManager.swift
@@ -1238,7 +1238,7 @@ open class CameraManager: NSObject, AVCaptureFileOutputRecordingDelegate, UIGest
     fileprivate func _createMovieOutput() {
         
         let newMovieOutput = AVCaptureMovieFileOutput()
-        newMovieOutput.maxRecordedDuration = CMTime(seconds: 15.0, preferredTimescale: 100000)
+        newMovieOutput.maxRecordedDuration = CMTime(seconds: 15.3, preferredTimescale: 100000)
         newMovieOutput.movieFragmentInterval = CMTime.invalid
 
         movieOutput = newMovieOutput
